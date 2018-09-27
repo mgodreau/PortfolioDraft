2e87ab16c522769f1c29ef11f8a380868b1fc69d
mgodreau <mgodreau@gmail.com>
2018-09-27 12:44:25 -0500

    Added a NavBar 	-created nav container 	- created an index.js with react-bootstrap nav stuff 	- linked navbar to index.js in app

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import NavBar from '../nav'
 
 const App = () => (
+
   <div>
+    <NavBar />
     <header>
       <Link to="/">Home</Link>
       <Link to="/about-mere">About</Link>
@@ -14,6 +17,7 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/about-mere" component={About} />
     </main>
+
   </div>
 )
 
